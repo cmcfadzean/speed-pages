5386d9aa9cdfcc3ee2d123e483cafb7efaadb3f2
cmcfadzean <court@kajabi.com>
2018-10-15 16:21:42 -0700

    alert styles

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181009053254) do
+ActiveRecord::Schema.define(version: 20181015042656) do
+
+  create_table "pages", force: :cascade do |t|
+    t.string   "title"
+    t.string   "url"
+    t.boolean  "status"
+    t.integer  "user"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
